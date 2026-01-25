0160a41a9b9618af6774d72eb1d0a84815efa37c
Irfan Yulianto <irfan@dalfin.co.id>
2026-01-25 22:02:08 +0700

    Update server port configuration to use environment variable

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -291,9 +292,14 @@ func main() {
 		})
 	})
 
-	fmt.Println("Server running di localhost:8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-	err := http.ListenAndServe(":8080", nil)
+	fmt.Printf("Server running di port %s\n", port)
+
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		fmt.Println("gagal running server")
 	}
